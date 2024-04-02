91763358d1006499a112c8d6879a563dd2500385
Jose Fegasu <fegasu@misena.edu.co>
2024-04-02 11:20:18 -0500

    .

--- apirest/multinivel.py
+++ apirest/multinivel.py
@@ -8,5 +8,17 @@ def index():
 @app.route("/niveles/<id>",methods=["GET","POST"])
 def nivel(id=0):
     return render_template("niveles.html",N=id)
+
+@app.route("/niveles/i",methods=["POST"])
+def nivelInserta():
+    nom=request.form.get('nom')
+    ape=request.form.get('ape')
+    datos={
+        "NOMBRE":nom,"APELLIDO":ape
+    }
+    api_url="http://127.0.0.1:5000/usua/i"
+    response = requests.post(api_url, json=datos)
+    return nom
+
 if __name__=='__main__':
     app.run(debug=True,port=8000)

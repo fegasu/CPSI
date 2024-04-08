68f38eba5f59424d81c102b78eba1c098367d749
Jose Fegasu <fegasu@misena.edu.co>
2024-04-07 20:19:23 -0500

    Update inicio.bat

--- inicio.bat
+++ inicio.bat
@@ -5,5 +5,5 @@ start env\Scripts\Activate.bat
 cls
 echo CENTRO DE GESTION DE MERCADOS, LOGISTICA Y TECNOLOGIAS DE LA INFORMACION
 echo Centro de Produción de Soluciones Inteligentes CPSI
-pause
+
 

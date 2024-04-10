ebf99c686762aef8f8a19ac001bfd2e06d99e0c1
fegasu <fegasu@misena.edu.co>
2024-04-10 08:36:56 -0500

    .

--- envia.py
+++ envia.py
@@ -1,3 +1,11 @@
-import json,requests
-a=requests.get("cod")
-print(a)
+from flask import Flask, jsonify,requests
+import json
+
+app=Flask(__name__)
+@app.route("/p/<id>",methods=['GET'])
+def lea(id):
+    a=requests.get("cod")
+    print(a) 
+
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0')

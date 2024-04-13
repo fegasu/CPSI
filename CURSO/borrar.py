9bbf2620907eec20b3489ef16554a87245f88fbb
Jose Fegasu <fegasu@misena.edu.co>
2024-04-13 15:06:37 -0500

    .

--- CURSO/borrar.py
+++ CURSO/borrar.py
@@ -1,18 +1,17 @@
-import random
-class Persona:
-  def __init__(self, nombre, edad):
-    self.nombre = nombre
-    self.edad = edad
-
-  @staticmethod    #este es un método estático
-  def mensaje_hoy():
-    mensajes =["La vida es bella", "Mejor tarde que nunca", "Dios te bendiga", "Vive para servir"]
-    print (random.choice(mensajes))
-
-  def mostrar_info(self):
-    print (f"{self.nombre} tiene {self.edad} años")
-####################################################################################################
-Persona.mensaje_hoy()
-p1 = Persona ("Pablo", 38)
-p1.mostrar_info()
-Persona.mensaje_hoy()
+class Motor:
+  def __init_(self,tipo):
+    self.tipo=tipo
+  def arrancar(self):
+    print("Motor arrancado")
+  
+class Coche:
+  def __init__(self,modelo,tipoMotor):
+    self.__modelo=modelo
+    self.__tipoMotor=tipoMotor
+  def getModelo(self):
+        return self.__modelo
+  def conducir(self):
+    print("Conduciendo el coche modelo: "+self.getModelo())
+    
+miCoche=Coche("Sedan","Gasolina")
+miCoche.conducir()

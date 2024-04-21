a4c1a1d94909733d199426e6f85ff144e18fdc67
Jose Fegasu <fegasu@misena.edu.co>
2024-04-21 13:17:43 -0500

    .

--- todo/API/main.py
+++ todo/API/main.py
@@ -6,16 +6,34 @@ import json
 from api.cnxSqlite import cnxsqlite  
 from config import configura 
 app=Flask(__name__)
+bd="./db/horario.db"
 @app.route("/")
 def inicio():
     return "Hola"
-@app.route("/usua/cc")
-
-@app.route("/usua/to")
-def ListaUsuario():
-    sql="select * from USUA" 
+@app.route("/reg")
+def ListaRegional():
+    sql="select * from REGIONAL" 
     con=cnxsqlite()   
-    todo=con.Consultar("./usuarios.db",sql)
+    todo=con.Consultar(bd,sql)
     return json.dumps(todo)
+@app.route("/centro")
+def ListaCentro():
+    sql="select * from CENTRO" 
+    con=cnxsqlite()   
+    todo=con.Consultar(bd,sql)
+    return json.dumps(todo)
+@app.route("/sede")
+def ListaSede():
+    sql="select * from SEDE s, CENTRO c where s.idcentro=c.idcentro " 
+    con=cnxsqlite()   
+    todo=con.Consultar(bd,sql)
+    return json.dumps(todo)
+@app.route("/sede/<ids>")
+def ListaSede1(ids):
+    sql="select * from SEDE s, CENTRO c where s.idcentro=c.idcentro and s.idsede="+ids 
+    con=cnxsqlite()   
+    todo=con.Consultar(bd,sql)
+    return json.dumps(todo)
+
 if __name__=='__main__':
     app.run(debug=True,port=configura['PUERTOREST'],host='0.0.0.0')

f1c7167a6d1313d96a7fbb2662b4a69d9d1bca55
fegasu <fegasu@misena.edu.co>
2024-04-25 09:05:18 -0500

    .

---
+++ apirest/static/js/MiJs.js
@@ -0,0 +1,26 @@
+
+    function Ir(cual){
+        document.forms['mio'].action="/niveles/"+cual
+        document.forms['mio'].submit();
+     
+     
+       }function Ir1(cual){
+        location.href=cual
+       }
+        function Confirmar(que,donde){
+            if (confirm(que))
+              location.href=donde
+        }
+       function Va(tipo){
+        
+        if(tipo=="i")
+        document.forms['mio'].action="/niveles/i"
+        if(tipo=="u"){
+         
+         document.forms['mio'].action="/niveles/u"   
+        }
+        
+        document.forms['mio'].submit();    
+     
+       }
+   

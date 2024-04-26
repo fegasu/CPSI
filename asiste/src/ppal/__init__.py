a72cec68fe443260e5311633fb17f0d5dc6bce2f
Jose Fegasu <fegasu@misena.edu.co>
2024-04-25 20:08:00 -0500

    .

---
+++ asiste/src/ppal/__init__.py
@@ -0,0 +1,21 @@
+# imports
+from flask import Flask,render_template
+from flask_cors import CORS
+
+# Here we're gonna import the blueprints
+from ppal.routes import ppal
+
+
+# function than create a flask app with CORS
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    # Here we're gonna register the blueprints
+    app.register_blueprint(ppal)
+@ppal.route('/')
+def show():
+    return render_template("index.html")
+
+
+    return app

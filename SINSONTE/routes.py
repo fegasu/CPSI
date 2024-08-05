6db24ae64418fdd42b8226f717da57470c3688a4
fegasu <fegasu@misena.edu.co>
2024-08-05 18:52:23 -0500

    .

--- SINSONTE/routes.py
+++ SINSONTE/routes.py
@@ -9,6 +9,10 @@ def create_app():
 app = create_app() # CREATE THE FLASK APP
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    return render_template("otro.html")
+@app.route("/logo")
+def inilogocio():
+    return render_template("banner.html")
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)

5fa4fd48d16031726680e6479e78a5d708b3db40
fegasu <fegasu@misena.edu.co>
2024-08-05 18:41:53 -0500

    .

---
+++ SINSONTE/routes.py
@@ -0,0 +1,14 @@
+
+from flask import Flask,render_template,request
+from flask_cors import CORS
+def create_app():
+    app=Flask(__name__)
+    CORS(app)
+    return app
+
+app = create_app() # CREATE THE FLASK APP
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=5000)

59a109512f8691ef4ebea30b373b6c527881c6cf
fegasu <fegasu@misena.edu.co>
2024-08-21 17:53:44 -0500

    .

--- NOVEDADES/DESARROLLO/API/api.py
+++ NOVEDADES/DESARROLLO/API/api.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,redirect
 import requests
 from flask_cors import CORS
 import sqlite3
+import json
 
 def create_app():
     app=Flask(__name__)
@@ -10,9 +11,21 @@ def create_app():
 
 app = create_app() # CREATE THE FLASK APP
 
-@app.route("/ln/<tipo>")
-def listar(tipo):
-    return "Listar"
+@app.route("/ln")
+def listar():    
+    try:
+        con = sqlite3.connect("nov.db")
+    except:
+        return("Ocurrio un error")
+    
+    cur = con.cursor()
+    
+    sql="select * from novedades n, ambiente a  where estado=0 and a.idambiente=n.idambiente"
+    res=cur.execute(sql)
+    todo=res.fetchall()
+    con.close() 
+    return json.dumps(todo)
+    
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=8000)
